932d7e1ae57cc9403272740cb4084810f2cb3a14
sttandofac-svg <sttandofac@gmail.com>
2026-04-05 11:27:18 +0400

    Update user_states.py

--- states/user_states.py
+++ states/user_states.py
@@ -1,17 +1,19 @@
-# states/user_states.py
+# states/user_states.py  ← ЗАМЕНИ ПОЛНОСТЬЮ ЭТИМ КОДОМ
 from aiogram.fsm.state import State, StatesGroup
 
+class AccountStates(StatesGroup):
+    waiting_session_name = State()
+    waiting_api_id = State()
+    waiting_api_hash = State()
+
+class ChatStates(StatesGroup):
+    waiting_chat_input = State()
+    waiting_group_name = State()
+
 class BroadcastStates(StatesGroup):
-    waiting_for_session_name = State()
-    waiting_for_api_id = State()
-    waiting_for_api_hash = State()
-    
-    waiting_for_chat_input = State()      # добавить чат
-    waiting_for_group_name = State()
-    
     choosing_account = State()
-    choosing_chat_group_or_chats = State()
-    waiting_for_message_text = State()
-    waiting_for_photo = State()           # одно или альбом
-    waiting_for_delay = State()
-    confirm_broadcast = State()
+    choosing_chats = State()
+    waiting_text = State()
+    waiting_media = State()
+    waiting_delay = State()
+    confirm = State()
