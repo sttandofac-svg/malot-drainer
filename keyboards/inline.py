f795ee8e11139b5146854fbf7003baa2eb01851b
sttandofac-svg <sttandofac@gmail.com>
2026-04-05 11:35:46 +0400

    Update inline.py

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,6 +1,4 @@
-# keyboards/inline.py  ← СОЗДАЙ ПАПКУ keyboards И ФАЙЛ
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardMarkup
 
 def main_menu_kb():
     builder = InlineKeyboardBuilder()
@@ -10,9 +8,4 @@ def main_menu_kb():
     builder.button(text="➕ Добавить чат", callback_data="add_chat")
     builder.button(text="🚀 Новая рассылка", callback_data="new_broadcast")
     builder.adjust(2)
-    return builder.as_markup()
-
-def back_to_menu():
-    builder = InlineKeyboardBuilder()
-    builder.button(text="← Главное меню", callback_data="main_menu")
     return builder.as_markup()
