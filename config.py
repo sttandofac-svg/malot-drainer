3ad8d6b79059a80da3762e42dce2b7fd67522d9f
sttandofac-svg <sttandofac@gmail.com>
2026-04-05 10:28:10 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+# config.py
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+OWNER_ID = int(os.getenv("OWNER_ID"))  # Только этот user_id имеет доступ
+
+# Для Railway — все секреты берутся из Environment Variables
+API_TOKEN = os.getenv("BOT_TOKEN")  # токен бота от @BotFather
